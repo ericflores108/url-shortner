cf89661f2366855459f0034f1294a7bcfdf57910
ericflores108 <eflorty108@gmail.com>
2025-06-07 13:50:41 -0700

    refactor: shorten.go

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,13 +1,9 @@
 package server
 
 import (
-	"crypto/rand"
 	"encoding/json"
 	"log"
-	"log/slog"
-	"math/big"
 	"net/http"
-	"net/url"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
@@ -66,95 +62,4 @@ func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
 	if _, err := w.Write(resp); err != nil {
 		log.Printf("Failed to write response: %v", err)
 	}
-}
-
-type ShortenRequest struct {
-	URL string `json:"url"`
-}
-
-type ShortenResponse struct {
-	ShortURL    string `json:"short_url"`
-	ShortCode   string `json:"short_code"`
-	OriginalURL string `json:"original_url"`
-}
-
-func (s *Server) shortenHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "405 Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	var req ShortenRequest
-
-	err := json.NewDecoder(r.Body).Decode(&req)
-
-	if err != nil {
-		http.Error(w, "400 Bad Request", http.StatusBadRequest)
-		return
-	}
-
-	if req.URL == "" {
-		http.Error(w, "URL is required", http.StatusBadRequest)
-		return
-	}
-
-	if !isValidURL(req.URL) {
-		http.Error(w, "URL is not valid", http.StatusBadRequest)
-		return
-	}
-
-	shortCode, err := generateShortCode(8)
-	if err != nil {
-		http.Error(w, "Error generating short code", http.StatusInternalServerError)
-		return
-	}
-
-	response := ShortenResponse{
-		ShortURL:    "http://localhost:8080/" + shortCode,
-		ShortCode:   shortCode,
-		OriginalURL: req.URL,
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-
-	err = json.NewEncoder(w).Encode(response)
-	if err != nil {
-		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
-		return
-	}
-}
-
-func isValidURL(urlStr string) bool {
-	url, err := url.Parse(urlStr)
-	if err != nil {
-		slog.Warn("url.Parse", "err", err)
-		return false
-	}
-
-	if url.Scheme != "https" {
-		slog.Warn("invalid url scheme", "scheme", url.Scheme)
-		return false
-	}
-
-	if url.Host == "" {
-		slog.Warn("url.Host", "invalid", url.Host)
-		return false
-	}
-
-	return true
-}
-
-func generateShortCode(length int) (string, error) {
-	b := make([]byte, length)
-	charset := "abcdefghijklmnopqrstuvwxyz0123456789"
-
-	for i := range len(b) {
-		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
-		if err != nil {
-			return "", err
-		}
-		b[i] = charset[num.Int64()]
-	}
-
-	return string(b), nil
 }
